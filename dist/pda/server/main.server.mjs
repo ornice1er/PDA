2f6b08e0f1a86655e4420c3adedc27e02f2cb885
OrniceDev <ornihouss1@gmail.com>
2025-04-30 18:14:39 +0100

    fix(usager): MAJ env

--- dist/pda/server/main.server.mjs
+++ dist/pda/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BFBHVRBH.mjs";import"./chunk-QKT3P7GK.mjs";import"./chunk-HXOK7I4G.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-I33W7O7W.mjs";import"./chunk-QKT3P7GK.mjs";import"./chunk-HXOK7I4G.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/environments/environment.pprod.ts
+++ src/environments/environment.pprod.ts
@@ -3,5 +3,6 @@ export const environment = {
   API_SCHEME: 'https',
   API_DOMAIN: 'guv.mtfp-ctd.bj/api',
   API_FILE: 'guv.mtfp-ctd.bj',
+  API_MATACCUEIL_DOMAIN: 'mataccueil-api.mtfp-ctd.bj/api',
   API_VERSION: 'v1'
 };
