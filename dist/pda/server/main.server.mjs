a408c8974211ec30b6b1e542a9cc20fcaad7beff
OrniceDev <ornihouss1@gmail.com>
2025-05-14 13:44:02 +0100

    fix(connexion sso mataccueil): correction affichage nom

--- dist/pda/server/main.server.mjs
+++ dist/pda/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5DYAUFYV.mjs";import"./chunk-HUHC67QU.mjs";import"./chunk-DFCYNFRL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-B4IWI3S7.mjs";import"./chunk-HUHC67QU.mjs";import"./chunk-DFCYNFRL.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
