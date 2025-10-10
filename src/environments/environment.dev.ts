8372eeebcd14baa9345e1d9459ece9fa196e9f20
Ornice <ornihouss1@gmail.com>
2025-10-10 20:49:14 +0100

    deploy gouv

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,8 +4,8 @@ export const environment = {
     accessToken: 'pk.eyJ1Ijoic2NoYWRyYWMtc29kZSIsImEiOiJjbWQzNzdubzQwMXdiMmtzZG45NG16cTdzIn0.IdIFjYGQKRv7DBD1c9qfLw'
   },
   API_SCHEME: 'https',
-  API_DOMAIN: 'guv-dev.mtfp-ctd.bj/api',
-  API_FILE: 'guv-dev.mtfp-ctd.bj',
+  API_DOMAIN: 'guv.mtfp-ctd.bj/api',
+  API_FILE: 'guv.mtfp-ctd.bj',
   API_MATACCUEIL_DOMAIN: 'mataccueil-api.mtfp-ctd.bj/api',
   API_ESERVICE_DOMAIN: 'boeservices-api.mtfp-ctd.bj/api',
   API_MATACCUEIL_FILE: 'mataccueil-api.mtfp-ctd.bj',
--- src/environments/environment.pprod.ts
+++ src/environments/environment.pprod.ts
@@ -4,10 +4,10 @@ export const environment = {
     accessToken: 'pk.eyJ1Ijoic2NoYWRyYWMtc29kZSIsImEiOiJjbWQzNzdubzQwMXdiMmtzZG45NG16cTdzIn0.IdIFjYGQKRv7DBD1c9qfLw'
   },
   API_SCHEME: 'https',
-  API_DOMAIN: 'guv.mtfp-ctd.bj/api',
-  API_FILE: 'guv.mtfp-ctd.bj',
-  API_MATACCUEIL_DOMAIN: 'mataccueil-api.mtfp-ctd.bj/api',
-  API_ESERVICE_DOMAIN: 'boeservices-api.mtfp-ctd.bj/api',
-  API_MATACCUEIL_FILE: 'mataccueil-api.mtfp-ctd.bj',
+  API_DOMAIN: 'preprodmtfp.gouv.bj/pprod-guv/api',
+  API_FILE: 'preprodmtfp.gouv.bj/pprod-guv',
+  API_MATACCUEIL_DOMAIN: 'preprodmtfp.gouv.bj/pprod-mataccueilapi/api',
+  API_ESERVICE_DOMAIN: 'preprodmtfp.gouv.bj/pprod-boapi/api',
+  API_MATACCUEIL_FILE: 'preprodmtfp.gouv.bj/pprod-mataccueilapi',
   API_VERSION: 'v1'
 };
