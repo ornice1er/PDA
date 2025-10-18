992cb02159a480b1db0f46602543c613c5f47a07
Ornice <ornihouss1@gmail.com>
2025-10-18 08:53:12 +0100

    ifx fix data

--- dist/pda/server/main.server.mjs
+++ dist/pda/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2LADRT5T.mjs";import"./chunk-FX4GDNGW.mjs";import"./chunk-FIXJ2T4L.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-MFUCKAJB.mjs";import"./chunk-FX4GDNGW.mjs";import"./chunk-FIXJ2T4L.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
